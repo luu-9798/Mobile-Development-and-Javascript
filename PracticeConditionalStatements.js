66cd0a76dbaa3cfa41cde81dea366687c237e410
luu-9798 <trungluu9798@gmail.com>
2025-06-15 22:32:08 -0400

    Days of the week program as a switch statement

--- PracticeConditionalStatements.js
+++ PracticeConditionalStatements.js
@@ -9,3 +9,31 @@ if (age >= 65) {
 } else {
     console.log("The value of the age variable is not numerical")
 }
+
+//Days of the week program as a switch statement
+var day = "Sunday"
+switch (day) {
+        case "Monday":
+        console.log("Start the week with energy!");
+        break;
+    case "Tuesday":
+        console.log("Keep the momentum going.");
+        break;
+    case "Wednesday":
+        console.log("Midweek hustle!");
+        break;
+    case "Thursday":
+        console.log("Almost there.");
+        break;
+    case "Friday":
+        console.log("Finish strong!");
+        break;
+    case "Saturday":
+        console.log("Time to relax and recharge.");
+        break;
+    case "Sunday":
+        console.log("Prepare for the week ahead.");
+        break;
+    default:
+        console.log("There is no such day");
+}
