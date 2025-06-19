a92720cfcb3835c6ab205c5bd9b59c6c53185548
luu-9798 <trungluu9798@gmail.com>
2025-06-18 22:57:54 -0400

    console.log('---No match found at', i)

--- PracticingWithFunctions.js
+++ PracticingWithFunctions.js
@@ -2,6 +2,8 @@ function letterFinder(word, match) {
     for (let i = 0; i < word.length; ++i) {
         if (word[i] == match) {
             console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
         }
     }
 };
