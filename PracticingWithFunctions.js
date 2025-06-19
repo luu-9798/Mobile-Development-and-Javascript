6028dc3aca58f85d967197221dc579a9fd4a27c4
luu-9798 <trungluu9798@gmail.com>
2025-06-18 22:57:06 -0400

    console.log('Found the', match, 'at', i)

--- PracticingWithFunctions.js
+++ PracticingWithFunctions.js
@@ -1,7 +1,7 @@
 function letterFinder(word, match) {
     for (let i = 0; i < word.length; ++i) {
         if (word[i] == match) {
-            
+            console.log('Found the', match, 'at', i)
         }
     }
 };
